69423ffbe75457447b5dc7e204e9155e8c06c883
m-heim <MaximilianHeim@protonmail.com>
2022-10-14 21:19:48 +0200

    changes

--- gp-parsergen.py
+++ gp-parsergen.py
@@ -8,4 +8,8 @@ if __name__ == '__main__':
     if len(sys.argv) < 3:
         usage()
         sys.exit(1)
+    with open(sys.argv[1], "r") as f:
+        content = f.read()
+    content_splitted = content.split("\n")
+    print("compiling to" + " " + content_splitted[0][1:] + " " + "grammar")
     main()
